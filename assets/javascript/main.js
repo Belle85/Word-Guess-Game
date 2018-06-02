7c5ab7826d3ff14f05235af6b933c59ba8e3d259
Belle Chauviray <bebold.bebelle@gmail.com>
2018-06-02 13:58:39 -0400

    Wrote startGame function. It displays underscores for each letter of the word choosen by the computer.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,24 @@
 //Creates an array that lists out all of the options.
 var computerChoices = ["Patrick","Squidward","SpongeBob","Plankton","Bikini"];
+var correctGuess = [];
 var letterGuessed = [];
 var wins = 0;
+
+
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 console.log("Computer guess:" + computerGuess);
+
+function startGame(){
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    for (var i = 0; i < computerGuess.length; i++) {
+        correctGuess.push("_")
+        console.log("Pushed")
+    }
+    console.log("Guess: " + correctGuess.join(" "))
+    document.getElementById("currentWord").innerText = correctGuess.join(" ");
+}
+
+document.onkeyup = function(event) {
+    startGame()
+}
